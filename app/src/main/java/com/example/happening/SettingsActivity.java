2c258d777749a572a5d48a66d25fe035993fe98c
Elias Magnusson <magnusson.elias1@gmail.com>
2019-03-19 14:49:47 +0100

    Settings done

---
+++ app/src/main/java/com/example/happening/SettingsActivity.java
@@ -0,0 +1,58 @@
+package com.example.happening;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private Button logOutBtn;
+    private Button changePwBtn;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+
+        changePwBtn = (Button)findViewById(R.id.changePwBtn);
+        changePwBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent myIntent = new Intent(SettingsActivity.this, changePwActivity.class);
+                startActivityForResult(myIntent, 0);
+
+
+            }
+        });
+
+        logOutBtn = (Button)findViewById(R.id.logOutBtn);
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                auth.signOut();
+                LoginManager.getInstance().logOut();
+
+                Intent myIntent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivityForResult(myIntent, 0);
+
+                Toast.makeText(SettingsActivity.this, "Logged out", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
